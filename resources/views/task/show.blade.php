c25100051e62fdf0f306157dc57bff9773b0e0db
Jordan <thejordan.dev@gmail.com>
2024-02-04 16:10:06 +0100

    Traduction Task.show

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -5,12 +5,12 @@
 @section('content')
     <h1>{{ $task->name }}</h1>
 
-    <span><strong>Due Date:</strong> {{ $task->due_date }}</span><br>
+    <span><strong>Pour:</strong> {{ $task->due_date }}</span><br>
     @if ($task->category)
-        <span><strong>Category:</strong> {{ $task->category->name }}</span><br>
+        <span><strong>Catégorie:</strong> {{ $task->category->name }}</span><br>
     @endif
-    <span><strong>Completed:</strong> {{ $task->completed ? 'Yes' : 'No' }}</span><br>
-    <span><strong>Content:</strong></span><br>
+    <span><strong>Fini:</strong> {{ $task->completed ? 'Yes' : 'No' }}</span><br>
+    <span><strong>Contenu:</strong></span><br>
     <textarea readonly rows="8" style="width:100%">{{ $task->content }}</textarea><br>
 
     <a href="{{ route('task.edit', $task->id) }}" class="btn btn-primary">Edit</a>
